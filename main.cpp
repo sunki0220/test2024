f7b8beb006c28e475778caf8e5c356df83361860
sk <nighthhhh@naver.com>
2024-10-30 23:57:32 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>  // printf를 사용하기 위해 포함합니다.
+
+int main() {
+    int age = 25;
+    const char* name = "홍길동";
+    float height = 175.5;
+
+    // printf를 사용하여 출력
+    printf("이름: %s\n", name);
+    printf("나이: %d세\n", age);
+    printf("키: %.1fcm\n", height);
+
+    return 0;
+}
